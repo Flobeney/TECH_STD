467a2a1eaeeb9c6b47b2ac4a25f68ebfca390969
Flobeney <flobeney@users.noreply.github.com>
2020-11-07 00:38:49 +0100

    Essai asynchrone et parallèle

--- sort_big_data/sort_big_data/Program.cs
+++ sort_big_data/sort_big_data/Program.cs
@@ -10,13 +10,19 @@ namespace sort_big_data {
     class Program {
         //Constantes
         const string FILENAME = "file.txt";
+        //const string FILENAME = "lorem5.txt";
+        const int NB_LINES_TO_READ = 100000;
 
         //Champs
         static SortBigData sort;
         static StreamReader file;
         static string currentLine;
+        static string[] linesRead;
+        static int lines;
+        static List<Task> tasks;
 
         static void Main(string[] args) {
+            Console.WriteLine(DateTime.Now);
             //Vérification de l'existence du fichier
             if (!File.Exists(FILENAME)) {
                 throw new FileNotFoundException();
@@ -24,16 +30,48 @@ namespace sort_big_data {
 
             //Classe pour trier le fichier
             sort = new SortBigData();
+            //Lines lues
+            linesRead = new string[NB_LINES_TO_READ];
+            currentLine = "";
+            //Tâches pour l'asynchrone
+            tasks = new List<Task>();
 
             //Lecture du fichier
-            using(file = new StreamReader(FILENAME)) {
-                //Lecture d'une ligne
-                while ((currentLine = file.ReadLine()) != null) {
-                    sort.AddLine(currentLine);
+            using (file = new StreamReader(FILENAME)) {
+                while (currentLine != null) {
+                    Console.WriteLine($"Begin read {NB_LINES_TO_READ} lines");
+                    //Lire NB_LINES_TO_READ
+                    for (int i = 0; i < NB_LINES_TO_READ && (currentLine = file.ReadLine()) != null; i++) {
+                        linesRead[i] = currentLine;
+                        lines++;
+                    }
+                    tasks.Add(sort.AddLines(linesRead));
                 }
+                 
+                //Lecture d'une ligne
+                //while ((currentLine = file.ReadLine()) != null) {
+                //    sort.addline(currentline);
+                //}
             }
 
+            //Attendre que toutes les lignes soient ajoutées dans leur fichier respectif
+            Task.WaitAll(tasks.ToArray());
+            //while (tasks.Count > 0) {
+            //    Task finished = await Task.WhenAny(tasks);
+            //    Console.WriteLine("finished");
+            //    tasks.Remove(finished);
+
+            //}
+
+            Console.WriteLine($"File readed : {lines} lignes");
+
+            //Trier les mots dans les fichiers
             sort.SortAllFiles();
+
+            //Écrire dans le fichier de résultat
+            sort.WriteResFile();
+            Console.WriteLine(DateTime.Now);
         }
+
     }
 }
--- sort_big_data/sort_big_data/SortBigData.cs
+++ sort_big_data/sort_big_data/SortBigData.cs
@@ -7,41 +7,102 @@ using System.Threading.Tasks;
 
 namespace sort_big_data {
     class SortBigData {
+        //Constantes
+        private const string FILE_RES = "res.txt";
+        private readonly string FOLDER_DATA = "data_" + DateTime.Now.Ticks.ToString() + "/";
+        private const char WORD_SEPARATOR = ' ';
+
         //Champs
-        char[] alphabet;
-        Dictionary<char, FileStream> files;
-        UTF8Encoding encoding;
+        private char[] alphabet;
+        private List<FileStream> files;
+        private List<StreamReader> readers;
+        private FileStream fileRes;
+        private UTF8Encoding encoding;
+        private object objLock;
 
         /// <summary>
         /// Constructeur
         /// </summary>
         public SortBigData() {
+            //Supprimer le fichier de résultat s'il existe déjà
+            if (File.Exists(FILE_RES)) {
+                File.Delete(FILE_RES);
+            }
+            //Dossier des fichiers de données
+            if (Directory.Exists(FOLDER_DATA)) {
+                throw new Exception($"Le dossier {FOLDER_DATA} existe déjà");
+            }
+            Directory.CreateDirectory(FOLDER_DATA);
+            //Initialisation
             alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-            files = new Dictionary<char, FileStream>();
+            files = new List<FileStream>();
+            readers = new List<StreamReader>();
+            fileRes = File.Create(FILE_RES);
             encoding = new UTF8Encoding();
+            objLock = new object();
 
             Console.WriteLine("Create");
-            foreach (char c in alphabet) {
-                Console.WriteLine(c);
-                //Créer le fichier pour la lettre correspondante
-                files.Add(
-                    c,
-                    File.Create($"{c}.txt")
-                );
-            }
         }
 
         /// <summary>
         /// Finalizer
         /// </summary>
-        ~SortBigData() {
-            Console.WriteLine("Delete");
-            //Pour chaque lettres
-            foreach (char c in alphabet) {
-                Console.WriteLine(c);
-                //Supprimer le fichier
-                File.Delete($"{c}.txt");
-            }
+        //~SortBigData() {
+        //    Console.WriteLine("Delete");
+        //    //Pour chaque lettres
+        //    for (int nb = 0; nb < files.Count; nb++) {
+        //        Console.WriteLine(nb);
+        //        //Fermer les Streams
+        //        files[nb].Close();
+        //        //Supprimer le fichier
+        //        File.Delete(GetFilename(nb));
+        //    }
+        //    //Dossier des fichiers de données
+        //    Directory.Delete(FOLDER_DATA);
+        //}
+
+        /// <summary>
+        /// Ajouter plusieurs lignes dans un fichier
+        /// </summary>
+        /// <param name="lines"></param>
+        public async Task AddLines(string[] lines) {
+            Console.WriteLine($"Begin add lines to files {files.Count}.txt");
+            List<string>[] words = new List<string>[lines.Length];
+            string[] res = new string[lines.Length];
+            //Créer le fichier
+            files.Add(
+                File.Create(
+                    GetFilename(files.Count)
+                )
+            );
+            int cpt = files.Count - 1;
+            readers.Add(new StreamReader(files[cpt]));
+            //Lancer l'exécution de la lecture du mot
+            await Task.Run(() => {
+                Console.WriteLine($"start task on {cpt}.txt");
+                //Parcourir les lignes
+                Parallel.For(0, lines.Length, i => {
+                    words[i] = new List<string>();
+                    res[i] = string.Empty;
+                    //Séparer la ligne en mots
+                    words[i] = lines[i].Split(WORD_SEPARATOR).ToList();
+
+                    //Trier les mots
+                    words[i].Sort();
+
+                    //Ajouter les mots dans le résultat
+                    foreach (string word in words[i]) {
+                        res[i] += word + Environment.NewLine;
+                    }
+
+                    lock (objLock) {
+                        //Ajouter les mots au fichier correspondant
+                        Write(files[cpt], res[i]);
+                    }
+                });
+                Console.WriteLine($"end task on {cpt}.txt");
+            });
+            Console.WriteLine($"Finish adding lines on {cpt}.txt");
         }
 
         /// <summary>
@@ -50,7 +111,7 @@ namespace sort_big_data {
         /// <param name="line">La ligne à ajouter</param>
         public void AddLine(string line) {
             //Séparer la ligne en mots
-            string[] words = line.Split(' ');
+            string[] words = line.Split(WORD_SEPARATOR);
 
             //Parcourir les mots de la ligne
             foreach (string word in words) {
@@ -63,25 +124,59 @@ namespace sort_big_data {
         /// Trier tous les fichiers
         /// </summary>
         public void SortAllFiles() {
-            foreach (char c in alphabet) {
-                SortFile(files[c]);
+            Console.WriteLine("start sort");
+            Parallel.For(0, files.Count, i => {
+                SortFile(i);
+            });
+            Console.WriteLine("end sort");
+        }
+
+        /// <summary>
+        /// Écrire le contenu des fichiers de données dans le fichier de résultat
+        /// </summary>
+        public void WriteResFile() {
+            for (int i = 0; i < readers.Count; i++) {
+                WriteFile(i);
+            }
+        }
+
+        /// <summary>
+        /// Écrire le contenu du fichier donné dans le fichier de résultat
+        /// </summary>
+        /// <param name="nb">Le chiffre du fichier de donnée</param>
+        private void WriteFile(int nb) {
+            string currentLine;
+
+            //Lire à partir du début
+            readers[nb].BaseStream.Position = 0;
+            readers[nb].DiscardBufferedData();
+
+            //Lire ligne par ligne
+            while ((currentLine = readers[nb].ReadLine()) != null) {
+                //Écrire dans le fichier de résultat
+                Write(fileRes, currentLine + Environment.NewLine);
             }
         }
 
         /// <summary>
         /// Trier le fichier donné
         /// </summary>
-        /// <param name="fs">FileStream du fichier</param>
-        private void SortFile(FileStream fs) {
+        /// <param name="nb">Le chiffre du fichier de donnée</param>
+        private void SortFile(int nb) {
+            Console.WriteLine($"start sort on {nb}.txt");
             //Lecture des lignes
-            List<string> lines = ReadAllLines(fs);
+            List<string> lines = ReadAllLines(nb);
 
             //Tri des lignes
             lines.Sort();
 
             //Écrire par dessus le fichier
-            fs.Position = 0;
-            Write(fs, string.Join(Environment.NewLine, lines));
+            files[nb].Position = 0;
+            Write(files[nb], string.Join(Environment.NewLine, lines));
+
+            //Écrire dans le fichier de résultat
+            //Write(fileRes, string.Join(Environment.NewLine, lines));
+            Console.WriteLine($"end sort on {nb}.txt");
         }
 
         /// <summary>
@@ -100,21 +195,40 @@ namespace sort_big_data {
         /// <summary>
         /// Lecture de toutes les lignes d'un fichier
         /// </summary>
-        /// <param name="fs">FileStream du fichier</param>
+        /// <param name="nb">Le chiffre du fichier de donnée</param>
         /// <returns>Le contenu d'un fichier</returns>
-        private List<string> ReadAllLines(FileStream fs) {
-            //Lire à partir du début
-            fs.Position = 0;
-            StreamReader reader = new StreamReader(fs);
+        private List<string> ReadAllLines(int nb) {
             List<string> res = new List<string>();
-
             string currentLine;
+
+            //Lire à partir du début
+            readers[nb].BaseStream.Position = 0;
+            readers[nb].DiscardBufferedData();
+
             //Lire ligne par ligne
-            while ((currentLine = reader.ReadLine()) != null) {
+            while ((currentLine = readers[nb].ReadLine()) != null) {
                 res.Add(currentLine);
             }
 
             return res;
+        }
+
+        /// <summary>
+        /// Récupérer le nom complet d'un fichier de données
+        /// </summary>
+        /// <param name="c">La lettre du fichier</param>
+        /// <returns>Le nom complet du fichier de données</returns>
+        private string GetFilename(char c) {
+            return $"{FOLDER_DATA}{c}.txt";
+        }
+
+        /// <summary>
+        /// Récupérer le nom complet d'un fichier de données
+        /// </summary>
+        /// <param name="nb">Le numéro du fichier</param>
+        /// <returns>Le nom complet du fichier de données</returns>
+        private string GetFilename(int nb) {
+            return $"{FOLDER_DATA}{nb}.txt";
         }
     }
 }
